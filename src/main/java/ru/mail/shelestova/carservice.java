2c62c162d17228e5ad8c9e7993d6c0e8cd2cf1c5
Stanly0407 <Shelestova_S_V@mail.ru>
2019-11-14 16:48:21 +0300

    4

--- src/main/java/ru/mail/shelestova/carservice.java
+++ src/main/java/ru/mail/shelestova/carservice.java
@@ -1,6 +1,5 @@
 package ru.mail.shelestova;
 
-import java.beans.Statement;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,7 +9,7 @@ public class carservice {
     private static final String PASSWORD = "503911";
     private static final String URL = "jdbc:mysql://localhost:3306/carservice";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         Connection connection = null;
         Driver driver;
 
@@ -44,26 +43,24 @@ public class carservice {
                         String companyTitle = rs.getString(2);
                         String companyAdress = rs.getString(3);
                         lst.add(new HeadOffice(id, companyTitle, companyAdress));
+                        //создать лист снова
                     }
+                    System.out.println("Для того, чтобы узнать адрес главного офиса обслуживающей компании:\n" +
+                            "введите название компании (например, Lukoil, Gazprom) и нажмите <Enter>:");
                     if (lst.size() > 0) {
-                        System.out.println("Для того, чтобы узнать адрес главного офиса обслуживающей компании:\n" +
-                                "введите название компании (например, Lukoil, Gazprom) и нажмите <Enter>:");
-                        Scanner scan = new Scanner(System.in);
-                        String name = scan.nextLine();
+                        Scanner scan1 = new Scanner(System.in);
+                        String name = scan1.nextLine();
                         String search1 = lst.get(0).getCompanyTitle(); //не очень, т.к. м.б. много записей... тогда через цикл м.б....
                         String search2 = lst.get(1).getCompanyTitle();
 
                         if (name.equals(search1)) {
                             System.out.println(lst.get(0));
-                        }
-                        else if  (name.equals(search2)) {
+                        } else if (name.equals(search2)) {
                             System.out.println(lst.get(1));
                         } else {
                             System.out.println("Компания не обслуживает, либо неверно введено название компании.");
                         }
-                        scan.close();
-
-
+                        scan1.close();
 
                     } else {
                         System.out.println("Not found.");
@@ -84,6 +81,60 @@ public class carservice {
                     System.err.println("Statement не создан.");
                 }
             }
+            //здесь снова снова создаем Statement читаю базу данных и новый лист.
+                /* String SQL2 = "SELECT * FROM companies";
+                try {
+                statement2 = connection.prepareStatement(SQL2);
+                ResultSet rs2 = null;
+                try {
+                    rs2 = statement2.executeQuery();
+                    ArrayList<Service> lst2 = new ArrayList<>();
+                    while (rs2.next()) {
+                        int id = rs2.getInt(1); !!!!!!!!!!!!!
+                        String companyTitle = rs2.getString(2); !!!!!!!!!!
+                        String companyAdress = rs2.getString(3); !!!!!!!!!!!
+                        lst2.add(new HeadOffice(id, companyTitle, companyAdress)); !!!!!!!!!!!!!!!!!!!!!
+                        //создать лист снова
+                    }
+                    System.out.println("Для того, чтобы узнать адрес главного офиса обслуживающей компании:\n" +
+                            "введите название компании (например, Lukoil, Gazprom) и нажмите <Enter>:");
+                    if (lst2.size() > 0) {
+                        Scanner scan2 = new Scanner(System.in);
+                        String name = scan2.nextLine();
+
+                        String search1 = lst.get(0).getCompanyTitle(); //не очень, т.к. м.б. много записей... тогда через цикл м.б....
+                        String search2 = lst.get(1).getCompanyTitle();
+
+                        if (name.equals(search1)) {
+                            System.out.println(lst.get(0));
+                        } else if (name.equals(search2)) {
+                            System.out.println(lst.get(1));
+                        } else {
+                            System.out.println("Компания не обслуживает, либо неверно введено название компании.");
+                        }
+                        scan2.close();
+
+                    } else {
+                        System.out.println("Not found.");
+                    }
+                } finally {
+                    //здесь закрытие ResultSet
+                    if (rs2 != null) {
+                        rs2.close();
+                    } else {
+                        System.err.println("Ошибка во время чтения из базы данных.");
+                    }
+                     }
+            } finally {
+                //здесь закрытие Statement
+                if (statement != null) {
+                    statement.close();
+                } else {
+                    System.err.println("Statement не создан.");
+                }
+            }
+                    */
+
 
         } catch (SQLException ex) {
             System.err.println("Не удалось создать соединение:" + ex);
